a41d7517284f50f826da672d766d1e887d6503c9
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2024-11-15 17:22:33 +0100

    feat(nccl): add high-level KokkosComm functions using NCCL backend

--- src/KokkosComm/collective.hpp
+++ src/KokkosComm/collective.hpp
@@ -3,11 +3,30 @@
 
 #pragma once
 
-#include <utility>
+#include <KokkosComm/fwd.hpp>
+#include <KokkosComm/concepts.hpp>
+#include <KokkosComm/nccl/reduce.hpp>
 
 #include <Kokkos_Core.hpp>
 
-#include "fwd.hpp"
-#include "concepts.hpp"
+#include <utility>
+#include "KokkosComm/nccl/allgather.hpp"
+#include "KokkosComm/nccl/nccl.hpp"
 
-namespace KokkosComm {}  // namespace KokkosComm
+namespace KokkosComm::Experimental {
+
+template <KokkosView SendView, KokkosView RecvView, KokkosExecutionSpace ExecSpace = Kokkos::Cuda,
+          CommunicationSpace CommSpace = KokkosComm::Experimental::Nccl, KokkosComm::ReductionOp RedOp>
+auto reduce(const Handle<ExecSpace, CommSpace>& h, const SendView& sv, RecvView& rv, int root) -> Req<Nccl> {
+  nccl::Impl::reduce(h.space(), sv, rv, nccl::Impl::reduction_op_v<RedOp>, root, h.rank(), h.get_inner());
+  return Req<Nccl>(h.space().cuda_stream());
+}
+
+template <KokkosView SendView, KokkosView RecvView, KokkosExecutionSpace ExecSpace = Kokkos::Cuda,
+          CommunicationSpace CommSpace = KokkosComm::Experimental::Nccl>
+auto allgather(const Handle<ExecSpace, CommSpace>& h, const SendView& sv, RecvView& rv) -> Req<Nccl> {
+  nccl::Impl::allgather(h.space(), sv, rv, h.get_inner());
+  return Req<Nccl>(h.space().cuda_stream());
+}
+
+}  // namespace KokkosComm::Experimental
