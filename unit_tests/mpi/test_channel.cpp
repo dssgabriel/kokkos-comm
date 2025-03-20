b770ce3fd37fee7ba0844de6f6f561cc023f980a
Nicole Avans <cnicoleavans@live.com>
2025-03-20 10:59:49 -0700

    add test for device views

--- unit_tests/mpi/test_channel.cpp
+++ unit_tests/mpi/test_channel.cpp
@@ -33,7 +33,7 @@ using ScalarTypes = ::testing::Types<int, int64_t, float, double, Kokkos::comple
 TYPED_TEST_SUITE(ChannelSendRecv, ScalarTypes);
 
 template <typename Scalar>
-void test_channel_send_recv() {
+void test_channel_hostspace() {
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -72,6 +72,54 @@ void test_channel_send_recv() {
   EXPECT_EQ(errs, 0);
 }
 
-TYPED_TEST(ChannelSendRecv, 1D_contig_standard) { test_channel_send_recv<typename TestFixture::Scalar>(); }
+template <typename Scalar>
+void test_channel_execspace() {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  if (size < 2) {
+    GTEST_SKIP() << "This test requires at least 2 MPI processes";
+  }
+
+  const int dest_rank = (rank + 1) % size;         // send to next rank
+  const int src_rank  = (rank - 1 + size) % size;  // recv from prev rank
+  const int tag       = 42;
+
+  KokkosComm::Channel<> channel(dest_rank, src_rank, tag, MPI_COMM_WORLD);
+
+  const int N = 10;
+
+  // Create host view
+  Kokkos::View<Scalar*, Kokkos::HostSpace> recv_host("recv_host", N);
+  // Create device views
+  Kokkos::View<Scalar*, Kokkos::DefaultExecutionSpace> send_dev("send_dev", N);
+  Kokkos::View<Scalar*, Kokkos::DefaultExecutionSpace> recv_dev("recv_dev", N);
+
+  GTEST_LOG_(INFO) << "Kokkos Execution Space: " << Kokkos::DefaultExecutionSpace::name();
+  Kokkos::parallel_for(
+      "init_send_dev", N, KOKKOS_LAMBDA(int i) { send_dev(i) = static_cast<Scalar>(rank * N + i); });
+  Kokkos::fence();
+
+  channel.sendinit(send_dev);
+  channel.recvinit(recv_dev);
+
+  channel.start();
+  channel.wait();
+
+  Kokkos::deep_copy(recv_host, recv_dev);
+
+  int errs = 0;
+  for (int i = 0; i < N; i++) {
+    const Scalar expected = static_cast<Scalar>(src_rank * N + i);
+    if (recv_host(i) != expected) {
+      errs++;
+    }
+  }
+  EXPECT_EQ(errs, 0);
+}
+
+TYPED_TEST(ChannelSendRecv, 1D_contig_sendrecv_hostspace) { test_channel_hostspace<typename TestFixture::Scalar>(); }
+TYPED_TEST(ChannelSendRecv, 1D_contig_sendrecv_execspace) { test_channel_execspace<typename TestFixture::Scalar>(); }
 
 }  // namespace
