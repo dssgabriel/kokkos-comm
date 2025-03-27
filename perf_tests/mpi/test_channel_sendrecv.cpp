452cd9e02e86aa28a46332470a3a1108e9aa2848
Nicole Avans <cnicoleavans@live.com>
2025-03-27 16:33:33 -0400

    clang formatting on perf test

--- perf_tests/mpi/test_channel_sendrecv.cpp
+++ perf_tests/mpi/test_channel_sendrecv.cpp
@@ -20,12 +20,15 @@
 
 template <typename View>
 void channel_send_recv(benchmark::State &, MPI_Comm comm, int rank, const View &v) {
-  int size; MPI_Comm_size(MPI_COMM_WORLD, &size);
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
   const int dest_rank = (rank + 1) % size;         // send to next rank
   const int src_rank  = (rank - 1 + size) % size;  // recv from prev rank
   KokkosComm::Channel<> channel(dest_rank, src_rank, 42, comm);
-  channel.sendinit(v); channel.recvinit(v);
-  channel.start(); channel.wait();
+  channel.sendinit(v);
+  channel.recvinit(v);
+  channel.start();
+  channel.wait();
 }
 
 void benchmark_channel_sendrecv(benchmark::State &state) {
@@ -36,7 +39,7 @@ void benchmark_channel_sendrecv(benchmark::State &state) {
     state.SkipWithError("benchmark_sendrecv needs at least 2 ranks");
   }
 
-  using Scalar = double;
+  using Scalar    = double;
   using view_type = Kokkos::View<Scalar *>;
   view_type a("", 1000000);
 
