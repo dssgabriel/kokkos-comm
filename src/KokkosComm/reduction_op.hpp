5c56cf6ee1bae244811d2939824d937ad8c8c220
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2025-07-04 22:52:27 +0200

    refactor: revert to custom reduction op marker structs

--- src/KokkosComm/reduction_op.hpp
+++ src/KokkosComm/reduction_op.hpp
@@ -25,34 +25,54 @@
 
 namespace KokkosComm {
 
+struct BAnd {};
 template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::BAnd> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::BOr> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::LAnd> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::LOr> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::Max> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::MaxLoc> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::Min> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::MinLoc> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::MinMax> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::MinMaxLoc> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::Sum> : public std::true_type {};
-template <>
-struct KokkosComm::Impl::is_reduction_operator<Kokkos::Prod> : public std::true_type {};
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::BAnd> : public std::true_type {};
 
-/// Custom "marker" reducer for computing an average.
-///
-/// Does nothing on its own, but is meant to serve as a "marker" reduction operator when calling NCCL reductions.
+struct BOr {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::BOr> : public std::true_type {};
+
+struct LAnd {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::LAnd> : public std::true_type {};
+
+struct LOr {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::LOr> : public std::true_type {};
+
+struct Max {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::Max> : public std::true_type {};
+
+struct MaxLoc {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::MaxLoc> : public std::true_type {};
+
+struct Min {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::Min> : public std::true_type {};
+
+struct MinLoc {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::MinLoc> : public std::true_type {};
+
+struct MinMax {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::MinMax> : public std::true_type {};
+
+struct MinMaxLoc {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::MinMaxLoc> : public std::true_type {};
+
+struct Sum {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::Sum> : public std::true_type {};
+
+struct Prod {};
+template <>
+struct KokkosComm::Impl::is_reduction_operator<KokkosComm::Prod> : public std::true_type {};
+
 struct Average {};
 template <>
 struct KokkosComm::Impl::is_reduction_operator<Average> : public std::true_type {};
@@ -64,16 +84,16 @@ template <ReductionOperator RedOp>
 constexpr auto reduction_op() -> ncclRedOp_t {
   if constexpr (std::is_same_v<RedOp, Kokkos::Max>) {
     return ncclMax;
-  } else if constexpr (std::is_same_v<RedOp, Kokkos::Min>) {
+  } else if constexpr (std::is_same_v<RedOp, KokkosComm::Min>) {
     return ncclMin;
-  } else if constexpr (std::is_same_v<RedOp, Kokkos::Sum>) {
+  } else if constexpr (std::is_same_v<RedOp, KokkosComm::Sum>) {
     return ncclSum;
-  } else if constexpr (std::is_same_v<RedOp, Kokkos::Prod>) {
+  } else if constexpr (std::is_same_v<RedOp, KokkosComm::Prod>) {
     return ncclProd;
   } else if constexpr (std::is_same_v<RedOp, KokkosComm::Average>) {
     return ncclAvg;
   } else {
-    static_assert(std::is_void_v<RedOp>, "NCCL reduction operator not implemented");
+    static_assert(std::is_void_v<RedOp>, "nccl::reduction_op: operator not implemented");
     return ncclMax;  // unreachable
   }
 }
