263dfa81cfcc90e83064d2b8d70667c51f68838e
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2025-10-14 00:20:05 +0200

    test(nccl): add reduce test

---
+++ unit_tests/nccl/test_reduce.hpp
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
+
+#include <gtest/gtest.h>
+#include <Kokkos_Core.hpp>
+
+#include <KokkosComm/KokkosComm.hpp>
+
+#include "utils.hpp"
+
+namespace {
+
+template <typename T>
+class Reduce : public testing::Test {
+ public:
+  using Scalar = T;
+};
+
+using ScalarTypes = ::testing::Types<int, int64_t, float, double>;
+TYPED_TEST_SUITE(Reduce, ScalarTypes);
+
+/// Each rank fills its sendbuf[i] with `rank + i`
+/// operation is sum, so recvbuf[i] should be sum(0..size) + i * size
+template <typename Scalar>
+auto reduce_contig_1d() -> void {
+  auto nccl_ctx = test_utils::nccl::Ctx::init();
+  KokkosComm::Handle<ExecSpace, CommSpace> h(nccl_ctx.comm());
+  int root = 0;
+
+  int n_contrib = 10;
+  Kokkos::View<Scalar *> sv("sv", n_contrib);
+  Kokkos::View<Scalar *> rv;
+  if (h.rank() == root) {
+    Kokkos::resize(rv, h.size());
+  }
+
+  // Fill send buffer
+  Kokkos::parallel_for(
+      sv.extent(0), KOKKOS_LAMBDA(int i) { sv(i) = h.rank() + i; });
+
+  KokkosComm::reduce<KokkosComm::Sum>(h, sv, rv, root);
+
+  if (h.rank() == root) {
+    int errs;
+    Kokkos::parallel_reduce(
+        rv.extent(0),
+        KOKKOS_LAMBDA(int &i, int &lsum) {
+          Scalar acc = 0;
+          for (int r = 0; r < h.size(); ++r) {
+            acc += r + i;
+          }
+          lsum += rv(i) != acc;
+        },
+        errs);
+    ASSERT_EQ(errs, 0);
+  }
+}
+
+TYPED_TEST(Reduce, Contiguous1D) { reduce_contig_1d<typename TestFixture::Scalar>(); }
+
+}  // namespace
