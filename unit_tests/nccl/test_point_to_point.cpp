53b0c0a7cf6694246a0fac82a547bba3c4af76c9
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2025-10-14 11:55:26 +0200

    test(nccl): improve P2P test
    Signed-off-by: Gabriel Dos Santos <gabriel.dossantos@cea.fr>

--- unit_tests/nccl/test_point_to_point.cpp
+++ unit_tests/nccl/test_point_to_point.cpp
@@ -22,67 +22,67 @@ class PointToPoint : public testing::Test {
   using Scalar = T;
 };
 
-using ScalarTypes = ::testing::Types<float, double, int, unsigned, int64_t, uint64_t>;
+using ScalarTypes = ::testing::Types<int, int64_t, float, double>;
 
 TYPED_TEST_SUITE(PointToPoint, ScalarTypes);
 
 template <typename Scalar>
 auto p2p_1d_contig() -> void {
-  Kokkos::View<Scalar *> a("a", 1000);
-
   auto nccl_ctx = test_utils::nccl::Ctx::init();
-  KokkosComm::Handle<ExecSpace, CommSpace> h(nccl_ctx.comm());
+  KokkosComm::Handle h(ExecSpace(), nccl_ctx.comm());
   if (h.size() < 2) {
     GTEST_SKIP() << "Requires >= 2 ranks (" << h.size() << " provided)";
   }
+  int src = 0;
+  int dst = 1;
 
-  if (0 == h.rank()) {
-    int dst = 1;
+  Kokkos::View<Scalar *> v("v", 10'000);
+
+  if (h.rank() == src) {
+    // Prepare send buffer
     Kokkos::parallel_for(
-        a.extent(0), KOKKOS_LAMBDA(const int i) { a(i) = i; });
-    auto req = KokkosComm::send(h, a, dst);
+        Kokkos::RangePolicy(ExecSpace(), v.extent(0)), KOKKOS_LAMBDA(int i) { v(i) = i; });
+    auto req = KokkosComm::send(h, v, dst);
     KokkosComm::wait(req);
-  } else if (1 == h.rank()) {
-    int src  = 0;
-    auto req = KokkosComm::recv(h, a, src);
+  } else if (h.rank() == dst) {
+    auto req = KokkosComm::recv(h, v, src);
     KokkosComm::wait(req);
     int errs;
     Kokkos::parallel_reduce(
-        a.extent(0), KOKKOS_LAMBDA(const int &i, int &lsum) { lsum += a(i) != Scalar(i); }, errs);
+        v.extent(0), KOKKOS_LAMBDA(int &i, int &lsum) { lsum += v(i) != Scalar(i); }, errs);
     ASSERT_EQ(errs, 0);
   }
 }
 
 template <typename Scalar>
 auto p2p_1d_noncontig() -> void {
-  Kokkos::View<Scalar **, Kokkos::LayoutRight> b("a", 10, 10);
-  auto a = Kokkos::subview(b, Kokkos::ALL, 2);  // take column 2 (non-contiguous)
-
   auto nccl_ctx = test_utils::nccl::Ctx::init();
   KokkosComm::Handle<ExecSpace, CommSpace> h(nccl_ctx.comm());
   if (h.size() < 2) {
     GTEST_SKIP() << "Requires >= 2 ranks (" << h.size() << " provided)";
   }
+  int src = 0;
+  int dst = 1;
 
-  if (0 == h.rank()) {
-    int dst = 1;
+  Kokkos::View<Scalar **, Kokkos::LayoutRight> v("v", 100, 100);
+  auto sv = Kokkos::subview(v, Kokkos::ALL, 2);  // take column 2 (non-contiguous)
+
+  if (h.rank() == src) {
     Kokkos::parallel_for(
-        a.extent(0), KOKKOS_LAMBDA(const int i) { a(i) = i; });
-    KokkosComm::Req req = KokkosComm::send(h, a, dst);
+        Kokkos::RangePolicy(ExecSpace(), sv.extent(0)), KOKKOS_LAMBDA(int i) { sv(i) = i; });
+    KokkosComm::Req req = KokkosComm::send(h, sv, dst);
     KokkosComm::wait(req);
-  } else if (1 == h.rank()) {
-    int src  = 0;
-    auto req = KokkosComm::recv(h, a, src);
+  } else if (h.rank() == dst) {
+    auto req = KokkosComm::recv(h, sv, src);
     KokkosComm::wait(req);
     int errs;
     Kokkos::parallel_reduce(
-        a.extent(0), KOKKOS_LAMBDA(const int &i, int &lsum) { lsum += a(i) != Scalar(i); }, errs);
+        sv.extent(0), KOKKOS_LAMBDA(int &i, int &lsum) { lsum += sv(i) != Scalar(i); }, errs);
     ASSERT_EQ(errs, 0);
   }
 }
 
 TYPED_TEST(PointToPoint, Contiguous1D) { p2p_1d_contig<typename TestFixture::Scalar>(); }
-
 TYPED_TEST(PointToPoint, NonContiguous1D) { p2p_1d_noncontig<typename TestFixture::Scalar>(); }
 
 }  // namespace
