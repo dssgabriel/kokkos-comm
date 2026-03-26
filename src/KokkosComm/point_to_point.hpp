15ba1a04f729ea52f608368be1355a5f060d1dc9
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2026-03-26 19:05:41 +0100

    refactor(core): move MPI specialization of `Recv` in P2P header
    Signed-off-by: Gabriel Dos Santos <gabriel.dossantos@cea.fr>

--- src/KokkosComm/point_to_point.hpp
+++ src/KokkosComm/point_to_point.hpp
@@ -11,8 +11,8 @@
 #include "mpi/mpi_space.hpp"
 #include "mpi/communicator.hpp"
 #include "mpi/request.hpp"
-#include "mpi/irecv.hpp"
 #include "mpi/send.hpp"
+#include "mpi/recv.hpp"
 #endif
 #if defined(KOKKOSCOMM_ENABLE_NCCL)
 #include "nccl/nccl_space.hpp"
@@ -30,6 +30,12 @@ template <KokkosExecutionSpace Exec, KokkosView SendV>
 struct Send<MpiSpace, Exec, SendV> {
   static auto execute(Communicator<MpiSpace, Exec>& comm, const SendV& sv, int dst) -> Request<MpiSpace> {
     return mpi::isend(comm, sv, dst, mpi::Impl::P2P_TAG);
+  }
+};
+template <KokkosExecutionSpace Exec, KokkosView RecvV>
+struct Recv<MpiSpace, Exec, RecvV> {
+  static auto execute(Communicator<MpiSpace, Exec>& comm, const RecvV& rv, int src) -> Request<MpiSpace> {
+    return mpi::irecv(comm, rv, src, mpi::Impl::P2P_TAG);
   }
 };
 #endif
