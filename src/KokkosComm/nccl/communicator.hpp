dssgabriel/kokkos-comm

commit acb1fb2b8c0d43e04b3d96da3156a41f2f969414

refactor: rename and rework `Request`s impls (#216)
* refactor: rename `Req` to `Request`

* refactor(mpi): rework implementation of MPI requests

- Removed dead code (`ViewHolderBase`)
- Eliminated internal `Record` struct
  - Requests are now move-only:
    - Copy ctor/assign op are deleted
    - Wrapping in a `shared_ptr` is not needed anymore
    - Match the semantics of `MPI_Request` objects and how wrappers
      around them are implemented in other libs (e.g. kaMPIng)
- Added new member functions:
  - `request` (const ref)
  - `request_ptr` (both const and non-const ptr)
  - `wait` (carries impl)
  - `test` (carries impl)
  - Related friend functions call these directly
- Renamed callback registration:
  = ~~`call_after_mpi_wait`~~ -> `add_callback`
- Fixed `wait_all` and `wait_any`:
  - Construct a temporary container for holding `MPI_Request` objects
    and directly calling `MPI_Wait{all,any}`
- Improved error handling
- Added support for querying the request with `test`

* refactor(nccl): rework implementation of NCCL

- Removed dead code (`ViewHolderBase`)
- Changed internal request handle:
  - ~~`cudaStream_t`~~ -> `cudaEvent_t`
  - Create an event from a stream capture
  - Allows for synchronizing only up-to the recorded event,
    not the full stream (which avoids potentially waiting
    on work submitted _after_ the request was enqueued)
- Eliminated internal `Record` struct
  - Requests are now move-only:
    - Copy ctor/assign op are deleted
    - Wrapping in a `shared_ptr` is not needed anymore
- Added new member functions:
  - `request` (const ref)
  - `request_ptr` (both const and non-const ptr)
  - `wait` (carries impl)
  - `test` (carries impl)
  - Related friend functions call these directly
- Renamed callback registration:
  - ~~`get_inner`~~ -> `request`
  - ~~`call_after_wait`~~ -> `add_callback`
- Reworked `wait_all`:
  - Active-wait loop that polls the requests until all are completed
  - Same approach as used in `wait_any`

src/KokkosComm/nccl/recv.hpp
@@ -11,33 +11,32 @@
 #include <KokkosComm/datatype.hpp>
 #include "nccl_space.hpp"
 #include "handle.hpp"
-#include "req.hpp"
+#include "request.hpp"
 
-#include <KokkosComm/impl/contiguous.hpp>
 #include "impl/pack_traits.hpp"
-#include "impl/nccl_check.hpp"
+#include "impl/error_handling.hpp"
 
 namespace KokkosComm {
 namespace Experimental::nccl {
 
-namespace KC = KokkosComm;
-
 template <KokkosExecutionSpace ExecSpace, KokkosView RecvView>
-auto recv(const ExecSpace &space, RecvView &rv, int peer, ncclComm_t comm) -> Req<NcclSpace> {
+auto recv(const ExecSpace& space, RecvView& rv, int peer, ncclComm_t comm) -> Request<NcclSpace> {
   using T = typename RecvView::non_const_value_type;
   Kokkos::Tools::pushRegion("KokkosComm::Impl::recv");
 
-  Req<NcclSpace> req{space.cuda_stream()};
-  if (KC::is_contiguous(rv)) {
-    KC_NCCL_CHECK(
-        ncclRecv(KC::data_handle(rv), KC::span(rv), datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
+  Request<NcclSpace> req;
+  if (is_contiguous(rv)) {
+    KC_NCCL_CHECK(ncclRecv(data_handle(rv), span(rv), datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
   } else {
     using Packer = typename Impl::PackTraits<RecvView>::packer_type;
-    auto pckd_rv = KC::Impl::allocate_contiguous_for(space, "KC::nccl::recv pckd_rv", rv);
-    KC_NCCL_CHECK(ncclRecv(KC::data_handle(pckd_rv), KC::span(pckd_rv), datatype<NcclSpace, T>(), peer, comm,
-                           space.cuda_stream()));
-    Packer::unpack_into(space, rv, pckd_rv);
-    req.extend_view_lifetime(pckd_rv);
+    auto pckd_rv = Packer::allocate_packed_for(space, "pckd_rv", rv);
+    KC_NCCL_CHECK(
+        ncclRecv(data_handle(pckd_rv.view_), pckd_rv.count_, pckd_rv.datatype_, peer, comm, space.cuda_stream()));
+    req.capture_stream_state(space.cuda_stream());
+    req.add_callback([space, rv, pckd_rv]() {
+      Packer::unpack_into(space, rv, pckd_rv.view_);
+      space.fence("fence `pckd_rv` unpacking after NCCL call");
+    });
   }
   req.extend_view_lifetime(rv);
 
@@ -50,8 +49,8 @@ namespace Impl {
 
 template <KokkosView RecvView>
 struct Recv<RecvView, Kokkos::Cuda, Experimental::NcclSpace> {
-  static auto execute(Handle<Kokkos::Cuda, Experimental::NcclSpace> &h, RecvView sv, int peer)
-      -> Req<Experimental::NcclSpace> {
+  static auto execute(Handle<Kokkos::Cuda, Experimental::NcclSpace>& h, RecvView sv, int peer)
+      -> Request<Experimental::NcclSpace> {
     return Experimental::nccl::recv(h.space(), sv, peer, h.comm());
   }
 };

src/KokkosComm/nccl/request.hpp
@@ -1,0 +1,197 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
+
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <span>
+#include <vector>
+
+#include <cuda_runtime.h>
+
+#include <KokkosComm/concepts.hpp>
+#include <KokkosComm/fwd.hpp>
+#include "nccl_space.hpp"
+
+#include "impl/error_handling.hpp"
+
+namespace KokkosComm {
+
+/// @brief Request specialization for the NCCL communication space.
+template <>
+class Request<Experimental::NcclSpace> {
+ public:
+  using communication_space = Experimental::NcclSpace;
+  using request_type        = Experimental::NcclSpace::request_type;
+  using rank_type           = Experimental::NcclSpace::rank_type;
+
+  /// @brief Constructs a `Request`.
+  explicit Request() : request_(nullptr) {}
+
+  /// @brief Capture the state of a `cudaStream_t` for request encapsulation.
+  /// @param stream The stream to capture for request encapsulation.
+  auto capture_stream_state(cudaStream_t stream) noexcept -> void {
+    if (request_ != nullptr) {
+      KC_CUDA_CHECK(cudaEventDestroy(request_));
+    }
+    KC_CUDA_CHECK(cudaEventCreate(&request_, cudaEventDisableTiming));
+    KC_CUDA_CHECK(cudaEventRecord(request_, stream));
+  }
+
+  /// @brief Destructor.
+  ~Request() noexcept {
+    if (request_ != nullptr) {
+      KC_CUDA_CHECK(cudaEventDestroy(request_));
+    }
+  };
+
+  /// @brief Copy constructor is deleted because a `Request` can only be moved.
+  Request(const Request&) = delete;
+  /// @brief Copy assignment operator is deleted because a `Request` can only be moved.
+  auto operator=(const Request&) -> Request& = delete;
+  /// @brief Move constructor.
+  Request(Request&&) = default;
+  /// @brief Move assignment operator.
+  auto operator=(Request&&) -> Request& = default;
+
+  /// @return A reference to the underlying `cudaEvent_t` object.
+  [[nodiscard]] constexpr auto request() noexcept -> request_type& { return request_; }
+  /// @return A const reference to the underlying `cudaEvent_t` object.
+  [[nodiscard]] constexpr auto request() const noexcept -> const request_type& { return request_; }
+  /// @return A pointer to the underlying `cudaEvent_t` object.
+  [[nodiscard]] constexpr auto request_ptr() noexcept -> request_type* { return &request_; }
+  /// @return A const pointer to the underlying `cudaEvent_t` object.
+  [[nodiscard]] constexpr auto request_ptr() const noexcept -> const request_type* { return &request_; }
+
+  /// @brief Adds a function to a list of callbacks to be invoked after the request's completion.
+  /// @param cb The callback function to register.
+  auto add_callback(std::function<void()>&& cb) -> void { callbacks_.push_back(cb); }
+
+  /// @brief Captures a Kokkos View to extend its lifetime until the request's completion.
+  /// @tparam V A Kokkos View type.
+  /// @param view The Kokkos View to capture for lifetime extension.
+  template <KokkosView V>
+  auto extend_view_lifetime(const V& view) -> void {
+    // Unmanaged views don't own the underlying buffer, so no need to extend their lifetime
+    if (view.use_count() != 0) {
+      add_callback([view]() {});
+    }
+  }
+
+  /// @brief Waits on the request until completion of the associated operation.
+  auto wait() -> void {
+    cudaError_t err = cudaEventSynchronize(request_);
+    // FIXME: Do something smarter with `err` for better error reporting
+    nccl::fail_if(err != cudaSuccess, "KokkosComm::Request::wait: request completion failed");
+
+    execute_all_callbacks();
+  }
+
+  /// @brief Queries the request for the completion of the associated operation.
+  /// If the operation has completed, all callbacks are executed upon return, similarly to having called `wait`.
+  /// @return True if the request has completed or is null/inactive, false otherwise.
+  [[nodiscard]] auto test() -> bool {
+    cudaError_t err = cudaEventQuery(request_);
+    if (err == cudaSuccess) {
+      execute_all_callbacks();
+      return true;
+    } else if (err == cudaErrorNotReady) {
+      return false;
+    }
+
+    // FIXME: Do something smarter with `err` for better error reporting
+    nccl::fail_if(err != cudaSuccess, "KokkosComm::Request::wait: request completion failed");
+  }
+
+ private:
+  request_type request_;
+  std::vector<std::function<void()>> callbacks_;
+
+  /// @brief Executes all the callbacks registered on the request.
+  auto execute_all_callbacks() -> void {
+    for (auto& cb : callbacks_) {
+      cb();
+    }
+    callbacks_.clear();
+  }
+
+  friend auto wait(Request<communication_space>& request) -> void;
+  friend auto wait(Request<communication_space>&& request) -> void;
+  friend auto wait_all(std::span<Request<communication_space>> requests) -> void;
+  friend auto wait_any(std::span<Request<communication_space>> requests) -> std::optional<rank_type>;
+  friend auto test(Request<communication_space>& request) -> bool;
+};
+
+/// @brief Waits on the request until completion of the associated operation.
+/// @param request A reference on the request to wait for completion.
+inline auto wait(Request<Experimental::NcclSpace>& request) -> void { request.wait(); }
+/// @brief Waits on the request until completion of the associated operation.
+/// @param request An r-value reference on the request, consumed upon completion.
+inline auto wait(Request<Experimental::NcclSpace>&& request) -> void { request.wait(); }
+
+/// @brief Waits for completion of all passed requests.
+/// @param requests The list of requests to complete.
+inline auto wait_all(std::span<Request<Experimental::NcclSpace>> requests) -> void {
+  if (requests.empty()) {
+    return;
+  }
+
+  int remaining = requests.size();
+  // Poll until all requests are completed
+  //
+  // NOTE: While this is an active-wait loop, it should be the best compromise for performance.
+  // Other implementation strategies could be:
+  // - Complete requests in parallel by spawning threads
+  // - Complete requests one at a time in a sequential loop
+  while (remaining > 0) {
+    for (auto& req : requests) {
+      cudaError_t err = cudaEventQuery(req.request());
+      if (err == cudaSuccess) {
+        req.execute_all_callbacks();
+        remaining--;
+      } else if (err == cudaErrorNotReady) {
+        continue;
+      } else {
+        // FIXME: Do something smarter with `err` for better error reporting
+        nccl::fail_if(err != cudaSuccess, "KokkosComm::Request::wait_all: request completions failed");
+      }
+    }
+  }
+}
+
+/// @brief Waits for the completion of one request among all passed requests.
+/// @param requests The list of requests to try to complete.
+/// @return The index of the request within the passed list upon successful completion, `std::nullopt` otherwise.
+inline auto wait_any(std::span<Request<Experimental::NcclSpace>> requests)
+    -> std::optional<typename Request<Experimental::NcclSpace>::rank_type> {
+  if (requests.empty()) {
+    return std::nullopt;
+  }
+
+  // Poll until at least one request is completed
+  //
+  // NOTE: While this is an active-wait loop, it should be the best compromise for simplicity/performance.
+  // Another implementation strategy could be to complete requests in parallel by spawning threads, but this needs
+  // synchronization on the first request completion.
+  while (true) {
+    for (size_t r = 0; r < requests.size(); ++r) {
+      cudaError_t err = cudaEventQuery(requests[r].request());
+      if (err == cudaSuccess) {
+        requests[r].execute_all_callbacks();
+        return static_cast<typename Request<Experimental::NcclSpace>::rank_type>(r);
+      } else if (err == cudaErrorNotReady) {
+        continue;
+      } else {
+        // FIXME: Do something smarter with `err` for better error reporting
+        nccl::fail_if(err != cudaSuccess, "KokkosComm::Request::wait_any: request completion failed");
+      }
+    }
+  }
+}
+
+/// @brief Queries the request for completion of the associated operation.
+/// @param request A reference on the request to query its completion.
+inline auto test(Request<Experimental::NcclSpace>& request) -> bool { request.test(); }
+
+}  // namespace KokkosComm

src/KokkosComm/nccl/send.hpp
@@ -11,31 +11,29 @@
 #include <KokkosComm/datatype.hpp>
 #include "nccl_space.hpp"
 #include "handle.hpp"
-#include "req.hpp"
+#include "request.hpp"
 
 #include "impl/pack_traits.hpp"
-#include "impl/nccl_check.hpp"
+#include "impl/error_handling.hpp"
 
 namespace KokkosComm {
 namespace Experimental::nccl {
 
-namespace KC = KokkosComm;
-
 template <KokkosExecutionSpace ExecSpace, KokkosView SendView>
-auto send(const ExecSpace& space, const SendView& sv, int peer, ncclComm_t comm) -> Req<NcclSpace> {
+auto send(const ExecSpace& space, const SendView& sv, int peer, ncclComm_t comm) -> Request<NcclSpace> {
   using T = typename SendView::non_const_value_type;
   Kokkos::Tools::pushRegion("KokkosComm::Impl::send");
 
-  Req<NcclSpace> req{space.cuda_stream()};
-  if (KC::is_contiguous(sv)) {
-    KC_NCCL_CHECK(
-        ncclSend(KC::data_handle(sv), KC::span(sv), datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
+  Request<NcclSpace> req;
+  if (is_contiguous(sv)) {
+    KC_NCCL_CHECK(ncclSend(data_handle(sv), span(sv), datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
   } else {
     using Packer = typename Impl::PackTraits<SendView>::packer_type;
-    auto args    = Packer::pack(space, sv);
+    auto pckd_sv = Packer::pack(space, "pckd_sv", sv);
     KC_NCCL_CHECK(
-        ncclSend(KC::data_handle(args.view_), args.count_, datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
-    req.extend_view_lifetime(args.view_);
+        ncclSend(data_handle(pckd_sv.view_), pckd_sv.count_, pckd_sv.datatype_, peer, comm, space.cuda_stream()));
+    req.capture_stream_state(space.cuda_stream());
+    req.extend_view_lifetime(pckd_sv.view_);
   }
   req.extend_view_lifetime(sv);
 
@@ -49,7 +47,7 @@ namespace Impl {
 template <KokkosView SendView>
 struct Send<SendView, Kokkos::Cuda, Experimental::NcclSpace> {
   static auto execute(Handle<Kokkos::Cuda, Experimental::NcclSpace>& h, SendView sv, int peer)
-      -> Req<Experimental::NcclSpace> {
+      -> Request<Experimental::NcclSpace> {
     return Experimental::nccl::send(h.space(), sv, peer, h.comm());
   }
 };

src/KokkosComm/mpi/request.hpp
@@ -1,0 +1,177 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
+
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <span>
+#include <vector>
+
+#include <mpi.h>
+
+#include <KokkosComm/concepts.hpp>
+#include <KokkosComm/fwd.hpp>
+#include "mpi_space.hpp"
+
+#include "impl/error_handling.hpp"
+
+namespace KokkosComm {
+
+/// @brief Request specialization for the MPI communication space.
+template <>
+class Request<MpiSpace> {
+ public:
+  using communication_space = MpiSpace;
+  using request_type        = MpiSpace::request_type;
+  using rank_type           = MpiSpace::rank_type;
+
+  /// @brief Constructs a `Request` from an `MPI_Request`.
+  /// @param request The request to encapsulate. Defaults to `MPI_REQUEST_NULL`.
+  explicit Request(request_type request = MPI_REQUEST_NULL) : request_(request) {}
+  /// @brief Destructor.
+  ~Request() = default;
+
+  /// @brief Copy constructor is deleted because a `Request` can only be moved.
+  Request(const Request&) = delete;
+  /// @brief Copy assignment operator is deleted because a `Request` can only be moved.
+  auto operator=(const Request&) -> Request& = delete;
+  /// @brief Move constructor.
+  Request(Request&&) = default;
+  /// @brief Move assignment operator.
+  auto operator=(Request&&) -> Request& = default;
+
+  /// @return A reference to the underlying `MPI_Request` object.
+  [[nodiscard]] constexpr auto request() noexcept -> request_type& { return request_; }
+  /// @return A const reference to the underlying `MPI_Request` object.
+  [[nodiscard]] constexpr auto request() const noexcept -> const request_type& { return request_; }
+  /// @return A pointer to the underlying `MPI_Request` object.
+  [[nodiscard]] constexpr auto request_ptr() noexcept -> request_type* { return &request_; }
+  /// @return A const pointer to the underlying `MPI_Request` object.
+  [[nodiscard]] constexpr auto request_ptr() const noexcept -> const request_type* { return &request_; }
+
+  /// @brief Adds a function to a list of callbacks to be invoked after the request's completion.
+  /// @param cb The callback function to register.
+  auto add_callback(std::function<void()>&& cb) -> void { callbacks_.push_back(cb); }
+
+  /// @brief Captures a Kokkos View to extend its lifetime until the request's completion.
+  /// @tparam V A Kokkos View type.
+  /// @param view The Kokkos View to capture for lifetime extension.
+  template <KokkosView V>
+  auto extend_view_lifetime(const V& view) -> void {
+    // Unmanaged views don't own the underlying buffer, so no need to extend their lifetime
+    if (view.use_count() != 0) {
+      add_callback([view]() {});
+    }
+  }
+
+  /// @brief Waits on the request until completion of the associated operation.
+  /// The underlying `MPI_Request` object is set to `MPI_REQUEST_NULL` upon return.
+  auto wait() -> void {
+    MPI_Status status;
+    int err = MPI_Wait(request_ptr(), &status);
+    // FIXME: Do something smarter with status` for better error handling and reporting
+    mpi::fail_if(err != MPI_SUCCESS, "KokkosComm::Request::wait: request completion failed");
+
+    execute_all_callbacks();
+  }
+
+  /// @brief Queries the request for the completion of the associated operation.
+  /// If the operation has completed, all callbacks are executed and the underlying `MPI_Request` object is set to
+  /// `MPI_REQUEST_NULL` upon return, similarly to having called `wait`.
+  /// @return True if the request has completed or is null/inactive, false otherwise.
+  [[nodiscard]] auto test() -> bool {
+    int has_completed;
+    MPI_Status status;
+    int err = MPI_Test(request_ptr(), &has_completed, &status);
+    // FIXME: Do something smarter with status` for better error handling and reporting
+    mpi::fail_if(err != MPI_SUCCESS, "KokkosComm::Request::test: request query failed");
+
+    if (has_completed) {
+      execute_all_callbacks();
+    }
+    return static_cast<bool>(has_completed);
+  }
+
+ private:
+  request_type request_;
+  std::vector<std::function<void()>> callbacks_;
+
+  /// @brief Executes all the callbacks registered on the request.
+  auto execute_all_callbacks() -> void {
+    for (auto& cb : callbacks_) {
+      cb();
+    }
+    callbacks_.clear();
+  }
+
+  friend auto wait(Request<communication_space>& request) -> void;
+  friend auto wait(Request<communication_space>&& request) -> void;
+  friend auto wait_all(std::span<Request<communication_space>> requests) -> void;
+  friend auto wait_any(std::span<Request<communication_space>> requests) -> std::optional<rank_type>;
+  friend auto test(Request<communication_space>& request) -> bool;
+};
+
+/// @brief Waits on the request until completion of the associated operation.
+/// @param request A reference on the request to wait for completion.
+inline auto wait(Request<MpiSpace>& request) -> void { request.wait(); }
+/// @brief Waits on the request until completion of the associated operation.
+/// @param request An r-value reference on the request, consumed upon completion.
+inline auto wait(Request<MpiSpace>&& request) -> void { request.wait(); }
+
+/// @brief Waits for the completion of all passed requests.
+/// Incurs an overhead for copying the underlying `MPI_Request` objects to an intermediate container.
+/// @param requests The list of requests to complete.
+inline auto wait_all(std::span<Request<MpiSpace>> requests) -> void {
+  if (requests.empty()) {
+    return;
+  }
+
+  std::vector<MPI_Request> mpi_requests;
+  std::vector<MPI_Status> mpi_statuses;
+  mpi_requests.reserve(requests.size());
+  mpi_statuses.reserve(requests.size());
+  for (auto& req : requests) {
+    mpi_requests.push_back(req.request());
+  }
+  int err = MPI_Waitall(static_cast<int>(mpi_requests.size()), mpi_requests.data(), mpi_statuses.data());
+  // FIXME: Do something smarter with `statuses` for better error handling and reporting
+  mpi::fail_if(err != MPI_SUCCESS, "KokkosComm::Request::wait_all: request completions failed");
+
+  for (auto& req : requests) {
+    req.execute_all_callbacks();
+  }
+}
+
+/// @brief Waits for the completion of one request among all passed requests.
+/// Incurs an overhead for copying the underlying `MPI_Request` objects to an intermediate container.
+/// @param requests The list of requests to try to complete.
+/// @return The index of the request within the passed list upon successful completion, `std::nullopt` otherwise.
+inline auto wait_any(std::span<Request<MpiSpace>> requests) -> std::optional<typename Request<MpiSpace>::rank_type> {
+  if (requests.empty()) {
+    return std::nullopt;
+  }
+
+  std::vector<MPI_Request> mpi_requests(requests.size());
+  for (auto& req : requests) {
+    mpi_requests.push_back(req.request());
+  }
+  int idx;
+  MPI_Status status;
+  int err = MPI_Waitany(static_cast<int>(mpi_requests.size()), mpi_requests.data(), &idx, &status);
+  // FIXME: Do something smarter with `status` for better error handling and reporting
+  mpi::fail_if(err != MPI_SUCCESS, "KokkosComm::Request::wait_any: request completion failed");
+
+  if (idx == MPI_UNDEFINED) {
+    return std::nullopt;
+  }
+
+  requests[idx].execute_all_callbacks();
+  return static_cast<typename Request<MpiSpace>::rank_type>(idx);
+}
+
+/// @brief Queries the request for completion of the associated operation.
+/// @param request A reference on the request to query its completion.
+inline auto test(Request<MpiSpace>& request) -> bool { return request.test(); }
+
+}  // namespace KokkosComm

perf_tests/mpi/test_2d_halo.cpp
@@ -10,8 +10,8 @@
 void noop(benchmark::State, MPI_Comm) {}
 
 template <typename Space, typename View>
-void send_recv(benchmark::State &, MPI_Comm comm, const Space &space, int nx, int ny, int rx, int ry, int rs,
-               const View &v) {
+void send_recv(benchmark::State&, MPI_Comm comm, const Space& space, int nx, int ny, int rx, int ry, int rs,
+               const View& v) {
   KokkosComm::Handle<> h{space, comm};
 
   // 2D index of nbrs in minus and plus direction (periodic)
@@ -35,7 +35,7 @@ void send_recv(benchmark::State &, MPI_Comm comm, const Space &space, int nx, in
   auto ym1_s = Kokkos::subview(v, make_pair(1, nx + 1), 1, Kokkos::ALL);
   auto ym1_r = Kokkos::subview(v, make_pair(1, nx + 1), 0, Kokkos::ALL);
 
-  std::vector<KokkosComm::Req<>> reqs;
+  std::vector<KokkosComm::Request<>> reqs;
   // std::cerr << get_rank(rx, ry) << " -> " << get_rank(xp1, ry) << "\n";
   reqs.push_back(KokkosComm::send(h, xp1_s, get_rank(xp1, ry)));
   reqs.push_back(KokkosComm::send(h, xm1_s, get_rank(xm1, ry)));
@@ -51,9 +51,9 @@ void send_recv(benchmark::State &, MPI_Comm comm, const Space &space, int nx, in
   KokkosComm::wait_all(reqs);
 }
 
-void benchmark_2dhalo(benchmark::State &state) {
+void benchmark_2dhalo(benchmark::State& state) {
   using Scalar    = double;
-  using grid_type = Kokkos::View<Scalar ***, Kokkos::LayoutRight>;
+  using grid_type = Kokkos::View<Scalar***, Kokkos::LayoutRight>;
 
   // problem size per rank
   int nx     = 512;

src/KokkosComm/KokkosComm.hpp
@@ -15,7 +15,7 @@
 #include "mpi/channel.hpp"
 #include "mpi/comm_mode.hpp"
 #include "mpi/handle.hpp"
-#include "mpi/req.hpp"
+#include "mpi/request.hpp"
 
 #include "mpi/irecv.hpp"
 #include "mpi/isend.hpp"
@@ -36,7 +36,7 @@
 #include "nccl/nccl_space.hpp"
 
 #include "nccl/handle.hpp"
-#include "nccl/req.hpp"
+#include "nccl/request.hpp"
 
 #include "nccl/recv.hpp"
 #include "nccl/send.hpp"

commit e77e494e8cf434dbd7a4bf9367f27356766de77f

fix(nccl): correct missed errors & silence warnings (#232)
* fix(nccl): don't declare unused communicator

* fix(nccl): silence warnings w/ unreachable ret after fatal err

* fix(nccl): actually return the result of req `test` in free func

* fix(nccl): capture stream state in send/recv for all code paths

Previously, `capture_stream_state` was only called in the non-contiguous
(packed) view path, causing the stream state to not be captured for
contiguous views in NCCL Send and Recv operations.

This fixes a bug introduced in #216 that made the SNL NCCL CI fail on
the P2P test.

---------

Signed-off-by: Gabriel Dos Santos <gabriel.dossantos@cea.fr>

src/KokkosComm/nccl/communicator.hpp
@@ -59,7 +59,6 @@ class Communicator<Experimental::NcclSpace, Kokkos::Cuda> {
   [[nodiscard]] static auto duplicate_from_raw(
       const communicator_type comm, const execution_space& exec = execution_space{}
   ) noexcept -> std::optional<Communicator<communication_space, execution_space>> {
-    communicator_type new_comm;
     int rank;
     ncclCommUserRank(comm, &rank);
     return Communicator::split_from_raw(comm, 0, rank, exec);

src/KokkosComm/nccl/recv.hpp
@@ -33,12 +33,12 @@ auto recv(const ExecSpace& space, RecvView& rv, int peer, ncclComm_t comm) -> Re
     KC_NCCL_CHECK(
         ncclRecv(data_handle(pckd_rv.view_), pckd_rv.count_, pckd_rv.datatype_, peer, comm, space.cuda_stream())
     );
-    req.capture_stream_state(space.cuda_stream());
     req.add_callback([space, rv, pckd_rv]() {
       Packer::unpack_into(space, rv, pckd_rv.view_);
       space.fence("fence `pckd_rv` unpacking after NCCL call");
     });
   }
+  req.capture_stream_state(space.cuda_stream());
   req.extend_view_lifetime(rv);
 
   Kokkos::Tools::popRegion();

src/KokkosComm/nccl/request.hpp
@@ -102,6 +102,8 @@ class Request<Experimental::NcclSpace> {
 
     // FIXME: Do something smarter with `err` for better error reporting
     nccl::fail_if(err != cudaSuccess, "KokkosComm::Request::wait: request completion failed");
+    // unreachable
+    return false;
   }
 
  private:
@@ -192,6 +194,6 @@ inline auto wait_any(std::span<Request<Experimental::NcclSpace>> requests)
 
 /// @brief Queries the request for completion of the associated operation.
 /// @param request A reference on the request to query its completion.
-inline auto test(Request<Experimental::NcclSpace>& request) -> bool { request.test(); }
+inline auto test(Request<Experimental::NcclSpace>& request) -> bool { return request.test(); }
 
 }  // namespace KokkosComm

src/KokkosComm/nccl/send.hpp
@@ -33,9 +33,9 @@ auto send(const ExecSpace& space, const SendView& sv, int peer, ncclComm_t comm)
     KC_NCCL_CHECK(
         ncclSend(data_handle(pckd_sv.view_), pckd_sv.count_, pckd_sv.datatype_, peer, comm, space.cuda_stream())
     );
-    req.capture_stream_state(space.cuda_stream());
     req.extend_view_lifetime(pckd_sv.view_);
   }
+  req.capture_stream_state(space.cuda_stream());
   req.extend_view_lifetime(sv);
 
   Kokkos::Tools::popRegion();